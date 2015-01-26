605613cb18d7c1a5c99157c18a30678c36528012
Reza Aleyasin <r.aleyasin@gmail.com>
2015-01-26 12:04:27 +0330

    Hotel forms

--- hotels/views.py
+++ hotels/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, render_to_response
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import UpdateView
 from django.views.generic.list import ListView
-from hotels.form import SearchByLocationForm, SearchByNameForm
+from hotels.form import SearchByLocationForm, SearchByNameForm, SearchByVoteForm
 from hotels.models import Hotel
 
 
@@ -22,7 +22,8 @@ class HotelSearchView(TemplateView):
     def get_context_data(self, **kwargs):
         return {
             'location_form': SearchByLocationForm(),
-            'name_form': SearchByNameForm()
+            'name_form': SearchByNameForm(),
+            'vote_form': SearchByVoteForm()
         }
 
     def post(self, request):
