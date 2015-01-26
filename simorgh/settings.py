2508f04f90786fe7462cf6aef250ed29159d90d3
Hossein Sekhavat <sekhavat17@gmail.com>
2015-01-26 10:10:46 +0330

    Add sitemap

--- simorgh/settings.py
+++ simorgh/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -37,6 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
     'bootstrap3',
     'reservation',
     'reports',
@@ -81,6 +84,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+SITE_ID = 1
 
 
 EMAIL_HOST = 'smtp.gmail.com'
--- simorgh/urls.py
+++ simorgh/urls.py
@@ -1,8 +1,27 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.sitemaps import Sitemap, GenericSitemap
+from django.core.urlresolvers import reverse
 from django.views.generic.base import TemplateView
+from hotels.models import Hotel
 import hotels.urls, accounts.urls, reservation.urls, reports.urls
 from hotels.views import HotelsList
+from django.contrib.sitemaps.views import sitemap
+
+
+class StaticViewsSitemap(Sitemap):
+    def items(self):
+        return ['home', 'hotel_list', 'login', 'register', 'accounts_edit', 'confirm', 'hotel_list', 'hotel_view',
+                'hotel_search', 'hotel_new']
+
+    def location(self, obj):
+        return reverse(obj)
+
+
+sitemaps = {
+    'static': StaticViewsSitemap,
+    'hotels': GenericSitemap(dict(queryset=Hotel.objects.all()))
+}
 
 urlpatterns = patterns(
     '',
@@ -10,7 +29,8 @@ urlpatterns = patterns(
     # url(r'^$', 'simorgh.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', TemplateView.as_view(template_name='home.html')),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hotels/', include(hotels.urls)),
     url(r'^accounts/', include(accounts.urls)),
