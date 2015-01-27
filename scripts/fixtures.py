7aa7f433fe25e7b10339809c6833a924e7d3ee3e
Hossein Sekhavat <sekhavat17@gmail.com>
2015-01-27 11:44:00 +0330

    Fixtures

--- scripts/fixtures.py
+++ scripts/fixtures.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
+import random
 from traceback import print_tb, print_exception
 from django.conf import settings
 from django.core.management import call_command
 import os
 from django.db import transaction
+from hotels.models import Hotel, RoomClass, Feature
 
 
 def run():
@@ -20,18 +23,74 @@ def run():
     @transaction.atomic()
     def gen():
         # UserFactory.create_batch(10)
-        # hotel_features = FeatureFactory.create_batch(10, is_for_hotel=True)
+        # hotel_feature_names = FeatureFactory.create_batch(10, is_for_hotel=True)
         # room_features = FeatureFactory.create_batch(10, is_for_room=True)
-        # HotelFactory.create_batch(10, features=hotel_features)
+        # HotelFactory.create_batch(10, features=hotel_feature_names)
         # RoomClassFactory.create_batch(10)
 
         from model_mommy import mommy
 
         import os
+
         cwd = os.getcwd()
         os.chdir('/tmp/')
-        for model in ['auth.User', 'hotels.Feature', 'hotels.Hotel', 'hotels.RoomClass', 'hotels.HotelImage']:
-            mommy.make(model, make_m2m=True, _quantity=10)
+        from django.contrib.auth.models import User
+
+        PASSWORD = 'admin'
+        admin = User.objects.create_superuser(
+            username='admin',
+            password=PASSWORD,
+            email='a@b.com',
+        )
+
+        names = [u'علی', u'تقی', u'نقی', u'سجاد', u'رضا']
+        users = [
+            User.objects.create_user(
+                username=('user%i' % i),
+                password=PASSWORD,
+                email='email@gmail.com',
+                first_name=names[i],
+                last_name=names[i] + u'یان',
+            ) for i in range(5)
+        ]
+
+        hotels = mommy.make_many(
+            Hotel,
+            2,
+            name=iter([u'هتل پارسیان', u'هتل هما', ]),
+            owner=iter(users),
+        )
+
+        feature_names = [u'لابی', u'آسانسور', u'لاندری', u'ماهواره', u'وای‌فای', u'آکواریوم', u'دشویی']
+        mommy.make_many(
+            Feature,
+            len(feature_names),
+            name=iter(feature_names),
+            is_for_hotel=True,
+            is_for_room=False,
+        )
+        mommy.make_many(
+            Feature,
+            len(feature_names),
+            name=iter(feature_names),
+            is_for_hotel=False,
+            is_for_room=True,
+        )
+
+        def random_features(is_for_hotel):
+            is_for_room = not is_for_hotel
+            return random.sample(list(Feature.objects.filter(is_for_hotel=is_for_hotel, is_for_room=is_for_room)), 3)
+
+
+        for hotel in hotels:
+            for i in range(3):
+                room_class = mommy.make_one(
+                    RoomClass,
+                    hotel=hotel,
+                    name=u'درجه ' + str(i),
+                    features=random_features(is_for_hotel=False)
+                )
+
         os.chdir(cwd)
 
 
